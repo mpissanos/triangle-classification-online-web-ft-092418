1f25ac71fdfdcb0877c1145a04e05f2db4c24b33
Manny Pissanos <mpissano@gmail.com>
2018-10-11 01:07:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind(triangle)
+    if valid1? && valid2?
+       triangle.
+      
+      binding.pry
+    end
+  
+  
+  def valid1?(side1, side2, side3)
+    side1.postive? && side2.positive? && side3.positve
+  end
+  
+  def valid2?(side1, side2, side3)
+    (side1 + side2) > side3
+  end
+  
+  class TriangleError < StandardError
+    
+  end
+
+  
 end
